3fd1c18033ea892c71066a9f16ae08b1ea7d8564
Alex Olde Monnikhof <aolde-mo@f0r2s14.codam.nl>
2023-05-23 19:41:30 +0200

    made norminette proof not sorted tho

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: aolde-mo <aolde-mo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 12:48:35 by aolde-mo          #+#    #+#             */
-/*   Updated: 2023/05/22 18:05:52 by aolde-mo         ###   ########.fr       */
+/*   Updated: 2023/05/23 19:39:43 by aolde-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,8 @@ typedef struct data{
 	unsigned int	time_to_eat;
 	unsigned int	time_to_sleep;
 	unsigned int	no_of_times_each_philosopher_must_eat;
-	unsigned long	start_time;
-	struct timeval	*tv;
+	long long		start_time;
+	struct timeval	tv;
 	pthread_t		*th;
 	t_philo			*philo;
 }			t_data;
@@ -43,6 +43,7 @@ typedef struct philo{
 	pthread_mutex_t	*fork;
 	pthread_mutex_t	*l_fork;
 	pthread_mutex_t	*r_fork;
+	bool			is_eating;
 }					t_philo;
 
 // typedef struct forks{
@@ -50,30 +51,31 @@ typedef struct philo{
 // 	pthread_mutex_t	*r_fork;
 // }					t_forks;
 
-void	parse_data(t_data *data, int argc, char **argv);
-void	parse_correct_arguments(t_data *data, int ms, int i);
-void	ft_parse(t_data *data, int argc, char **argv);
-void	parse_philo(t_data *data);
-void	parse_mutex(t_data *data);
+void		parse_data(t_data *data, int argc, char **argv);
+void		parse_correct_arguments(t_data *data, int ms, int i);
+void		ft_parse(t_data *data, int argc, char **argv);
+void		parse_philo(t_data *data);
+void		parse_mutex(t_data *data);
 
-void	ft_check(int argc, char **argv);
-void	check_argv(char **argv);
+void		arg_check(int argc, char **argv);
+void		check_argv(char **argv);
 
-void	ft_error(char *msg);
-void	ft_errno(char *msg);
+void		ft_error(char *msg);
+void		ft_errno(char *msg);
 
-int		ft_atoi_data(const char *str);
+int			ft_atoi_data(const char *str);
 
-void	ft_create_threads(t_data *data);
-void	ft_threads(t_data *data);
+void		create_and_join_threads(t_data *data);
 
 //ROUTINE
-void	*cycles(void *param);
-void	philo_eat(t_philo *philo);
-void	philo_think(void);
-void	philo_sleep(t_philo *philo);
+void		*cycles(void *param);
+void		philo_eat_and_sleep(t_philo *philo);
+void		philo_think(t_philo *philo);
 
 //TIME
-unsigned long	get_time(t_data *data);
+long long	get_time(t_data *data);
+void		acc_usleep(t_data *data, long long ms);
 
 #endif
+
+//ARGUMENTS IN ORDER: 1: PHILOS, 2: DIE, 3: EAT, 4: SLEEP, 5: TIMES_TO_EAT
