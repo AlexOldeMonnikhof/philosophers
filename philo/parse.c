2b005e9feca7d934a1cbbd9e94b8571440ddaa19
Alex Olde Monnikhof <aolde-mo@f1r1s8.codam.nl>
2023-05-26 20:25:18 +0200

    implemented return errors

--- philo/parse.c
+++ philo/parse.c
@@ -6,7 +6,7 @@
 /*   By: aolde-mo <aolde-mo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 12:58:48 by aolde-mo          #+#    #+#             */
-/*   Updated: 2023/05/23 19:36:35 by aolde-mo         ###   ########.fr       */
+/*   Updated: 2023/05/26 19:06:54 by aolde-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ void	parse_correct_arguments(t_data *data, int ms_or_no, int i)
 		data->times_philo_must_eat = ms_or_no;
 }
 
-void	parse_data(t_data *data, char **argv)
+int	parse_data(t_data *data, char **argv)
 {
 	int	i;
 	int	ms;
@@ -38,46 +38,49 @@ void	parse_data(t_data *data, char **argv)
 	while (argv[i])
 	{
 		ms = ft_atoi_data(argv[i]);
-		if (!ms)
-			ft_error("invalid argument(s)...");
+		if (ms == -1)
+			return (1);
 		parse_correct_arguments(data, ms, i);
 		i++;
 	}
 	data->status = malloc(sizeof(t_status));
-	// PROTECT!!!
+	if (!data->status)
+		return (1);
 	data->status->has_eaten_enough = 0;
 	data->status->all_ate = false;
 	data->status->someone_died = false;
 	gettimeofday(&data->tv, NULL);
 	data->start_time = data->tv.tv_sec * 1000LL + data->tv.tv_usec / 1000LL;
+	return (0);
 }
 
-void	parse_philo(t_data *data)
+int	parse_philo(t_data *data)
 {
-	t_philo	*philo;
 	int		i;
 	int		no;
 
 	i = 0;
 	no = data->no_of_philosophers;
 	data->th = malloc(no * sizeof(pthread_t));
+	if (!data->th)
+		return (1);
 	data->philo = malloc(no * sizeof(t_philo));
-	philo = data->philo;
-	if (!data->th || !philo)
-		exit(EXIT_FAILURE);
+	if (!data->philo)
+		return (2);
 	while (i < no)
 	{
-		philo[i].philo_no = i + 1;
-		philo[i].last_time_eaten = 0;
-		philo[i].times_eaten = 0;
-		philo[i].is_eating = false;
-		philo[i].data = data;
-		philo[i].fork = malloc(sizeof(pthread_mutex_t));
-		if (!philo[i].fork \
-			|| pthread_mutex_init(philo[i].fork, NULL))
-			exit(EXIT_FAILURE);
+		data->philo[i].philo_no = i + 1;
+		data->philo[i].last_time_eaten = 0;
+		data->philo[i].times_eaten = 0;
+		data->philo[i].is_eating = false;
+		data->philo[i].data = data;
+		data->philo[i].fork = malloc(sizeof(pthread_mutex_t));
+		if (!data->philo[i].fork \
+			|| pthread_mutex_init(data->philo[i].fork, NULL))
+			return (3 + i);
 		i++;
 	}
+	return (0);
 }
 
 void	parse_mutex(t_data *data)
@@ -95,9 +98,18 @@ void	parse_mutex(t_data *data)
 	}
 }
 
-void	ft_parse(t_data *data, char **argv)
+int	parse_all(t_data *data, char **argv)
 {
-	parse_data(data, argv);
-	parse_philo(data);
+	int	err;
+
+	if (parse_data(data, argv))
+		return (1);
+	err = parse_philo(data);
+	if (err)
+	{
+		free_specific(data, err);
+		return (1);
+	}
 	parse_mutex(data);
+	return (0);
 }
