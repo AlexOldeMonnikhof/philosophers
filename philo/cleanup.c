8a881efdfa4d790995146e91815f87f11147e876
Alex Olde Monnikhof <aolde-mo@f1r1s8.codam.nl>
2023-05-26 20:24:04 +0200

    added free_specific for parse

--- philo/cleanup.c
+++ philo/cleanup.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cleanup.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aolde-mo <aolde-mo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/26 16:34:08 by aolde-mo          #+#    #+#             */
+/*   Updated: 2023/05/26 19:08:39 by aolde-mo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 
 void	cleanup(t_data *data)
@@ -15,4 +27,19 @@ void	cleanup(t_data *data)
 	}
 	free(data->th);
 	free(data->philo);
+	free(data->status);
+}
+
+void	free_specific(t_data *data, int err)
+{
+	free(data->status);
+	if (err < 2)
+		return ;
+	if (err == 2)
+		free(data->th);
+	else
+	{
+		while (err >= 3)
+			free(data->philo[err-- - 3].fork);
+	}
 }
