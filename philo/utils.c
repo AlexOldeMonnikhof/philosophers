192007ccdb11435991186b9efb67858f4eddd8c2
Alex Olde Monnikhof <aolde-mo@f1r1s8.codam.nl>
2023-05-26 20:27:32 +0200

    removed errno added returns

--- philo/utils.c
+++ philo/utils.c
@@ -6,7 +6,7 @@
 /*   By: aolde-mo <aolde-mo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 13:22:26 by aolde-mo          #+#    #+#             */
-/*   Updated: 2023/05/17 20:13:56 by aolde-mo         ###   ########.fr       */
+/*   Updated: 2023/05/26 18:45:58 by aolde-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,11 @@ int	ft_atoi_data(const char *str)
 	while (str[i])
 	{
 		if (!(str[i] >= '0' && str[i] <= '9'))
-			return (0);
+			return (-1);
 		out = out * 10 + (str[i] - '0');
 		i++;
 	}
 	if (out > INT_MAX || out < 0)
-		ft_error("arguments greater than INT_MAX");
+		return (-1);
 	return (out);
 }
