8df5dbb1dc2d4494d6be173bb5aba626cd0f8074
Alex Olde Monnikhof <aolde-mo@f1r1s8.codam.nl>
2023-05-26 20:27:02 +0200

    removed errno added returns

--- philo/threads.c
+++ philo/threads.c
@@ -6,13 +6,13 @@
 /*   By: aolde-mo <aolde-mo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 15:35:56 by aolde-mo          #+#    #+#             */
-/*   Updated: 2023/05/23 19:33:30 by aolde-mo         ###   ########.fr       */
+/*   Updated: 2023/05/26 20:11:51 by aolde-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	create_and_join_threads(t_data *data)
+int	create_and_join_threads(t_data *data)
 {
 	unsigned int	i;
 	t_philo			*philo;
@@ -22,14 +22,15 @@ void	create_and_join_threads(t_data *data)
 	while (i < data->no_of_philosophers)
 	{
 		if (pthread_create(&data->th[i], NULL, &cycles, (void *)&philo[i]))
-			ft_errno("error creating thread...");
+			return (1);
 		i++;
 	}
 	i = 0;
 	while (i < data->no_of_philosophers)
 	{
 		if (pthread_join(data->th[i], NULL))
-			ft_errno("error joining thread...");
+			return (1);
 		i++;
 	}
+	return (0);
 }
